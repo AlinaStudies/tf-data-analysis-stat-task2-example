717065ecfd0a8cde6bfe267850b3e5861331ce0a
AlinaStudies <91690629+AlinaStudies@users.noreply.github.com>
2023-04-08 02:29:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,10 +11,10 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    df = 2 * x.shape[0]
+    df = 2 * len(x)
     r2 = np.dot(x, x) ** 0.5
     z1 = chi2.ppf(alpha / 2, df)
     z2 = chi2.ppf(1 - alpha / 2, df)
-    left = r2 / (13 * z2**0.5)
-    right = r2 / (13 * z1**0.5)
+    left = (r2 / (13 * z2))**0.5
+    right = (r2 / (13 * z1))**0.5
     return left, right
