00f3610c58a535a12c3dfa583ca0188312dbce47
AlinaStudies <91690629+AlinaStudies@users.noreply.github.com>
2023-04-08 03:06:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,14 +7,6 @@ from scipy.stats import chi2
 chat_id = 721973830 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    df = 2 * len(x)
-    r2 = sum(x ** 0.5)
-    z1 = chi2.ppf(alpha / 2, df)
-    z2 = chi2.ppf(1 - alpha / 2, df)
-    left = (r2 / (13 * z2))**0.5
-    right = (r2 / (13 * z1))**0.5
-    return left, right
+    length = len(x)
+    return np.sqrt(sum(x ** 2) / (13 * chi2.ppf((1 + p) / 2, df = 2 * length))), \
+           np.sqrt(sum(x ** 2) / (13 * chi2.ppf((1 - p) / 2, df = 2 * length)))
